5cd41fa4432ec6efb6fde0dbffadb5dc3b57e32e
JoaoFGuinguer <joaoguinguer@gmail.com>
2024-01-11 22:06:47 -0300

    add middleware, pinia and useState

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,17 @@
+<script setup>
+const user = useUser()
+</script>
 <template>
     <nav>
       <NuxtLink to="/">home    </NuxtLink>
       <NuxtLink to="/movies">movies</NuxtLink>
       <NuxtLink to="https://vueschool.io">Vue School</NuxtLink>
+      <NuxtLink v-if="!user.isLoggedIn" to="/login">Login</NuxtLink>
+      <a 
+        href="#" 
+        v-if="user.isLoggedIn" 
+        @click.prevent="user.isLoggedIn = false"
+      >Loggout</a>
       teste
     </nav>
     <slot></slot>
--- pages/login.vue
+++ pages/login.vue
@@ -1,10 +1,11 @@
 <script setup>
+const user = useUser()
 definePageMeta({
     layout: 'plain'
 })
 </script>
 <template>
-    <form>
+    <form @submit.prevent="user.login">
         <h1>Login</h1>
         <label> Username 
             <input type="text">
@@ -12,5 +13,6 @@ definePageMeta({
         <label> Passaword 
             <input type="password">
         </label>
+        <button>Login</button>
     </form>
 </template>
